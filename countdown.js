44a75e150f0b3571cb37ce493f28b25fe4e1d71b
Wayne <dev.wayne.ix@gmail.com>
2014-10-14 10:20:33 +0800

    f1

--- countdown.js
+++ countdown.js
@@ -1,8 +1,9 @@
 var countdown = function() {
-
+	this.hT = 60 * 60 * 1000;
+	this.mT = 60 * 1000;
+	this.sT = 1000;
 }
 
-var handle;
 
 var proto = countdown.prototype;
 
@@ -15,41 +16,76 @@ proto.convertDate = function(date) {
 		date = date.toString();
 	}
 	result = date.match(/\d{2}:\d{2}:\d{2}/).toString().split(":");
+	$.each(result, function(idx, val) {
+		val = parseInt(val);
+	});
 	return result;
 }
 
-proto.start = function(time,callback) {
+proto.start = function(date, callback) {
 	//	var $countdown = this.children();
-		if (!time) return;
-		if (true) {
-			var _arr = this.convertDate(time);
-			handle = setInterval(function() {
-				_now = new Date();
-				_arrnow = proto.convertDate(_now);
-				_h = (parseInt(_arr[0]) - parseInt(_arrnow[0])) - 1;
-				_m = 60 - (parseInt(_arrnow[1]) - parseInt(_arr[1]));
-				_s = 60 - (parseInt(_arrnow[2]) - parseInt(_arr[2]));
-				$('.transfer-hour').text(_h.toString());
-				$('.transfer-minute').text(_m.toString());
-				$('.transfer-second').text(_s.toString());
-				if(_h===0 && _m===0 && _s===0){
-					clearInterval(handle);
-					if (callback && (callback instanceof Function)) {
-							callback();
-						}
-				}
-			}, 1000)
-		} else {
+	var _h, _m, _s;
+	if (!date) return;
+	var time = date.getTime();
+	if (new Date().getTime() < time) {
 
+		function getHMS() {
+			_now = new Date().getTime();
+			delta = time - _now;
+
+			if (delta > 24 * 60 * 60 * 1000) {
+				return;
+			} else {
+				_h = _div(delta, this.hT);
+				_m = _div((delta - _h * this.hT), this.mT);
+				_s = _div((delta - _h * this.hT - _m * this.mT), this.sT);
+			}
+
+
+			$('.transfer-hour').text(_h.toString());
+			$('.transfer-minute').text(_m.toString());
+			$('.transfer-second').text(_s.toString());
+			if (_h === 0 && _m === 0 && _s === 0) {
+				clearInterval(handle);
+				if (callback && (callback instanceof Function)) {
+					callback();
+				}
+			}
 		}
+
+		function _div(exp1, exp2) {
+			var n1 = Math.round(exp1); //四舍五入   
+			var n2 = Math.round(exp2); //四舍五入  
+
+			var rslt = n1 / n2; //除  
+
+			if (rslt >= 0) {
+				rslt = Math.floor(rslt); //返回小于等于原rslt的最大整数。   
+			} else {
+				rslt = Math.ceil(rslt); //返回大于等于原rslt的最小整数。   
+			}
+
+			return rslt;
+		}
+
+		_this = this;
+
+		this.handle = setInterval(function() {
+			getHMS.apply(_this)
+		}, 1000)
+	} else {
+
+	}
+
+
 
 }
 
 proto.stop = function() {
-	clearInterval(handle);
+	clearInterval(this.handle);
 	return;
 }
 
-proto.checkTime=function(){
-	
+proto.checkTime = function() {
+
 }
